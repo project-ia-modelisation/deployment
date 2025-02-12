abb6049c5fbe2b3167527ec2032dc72e76596c4e
= <=>
2025-02-12 08:30:58 +0100

    modif_main

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-def greet(name):
-    return f"Hello deployment, {name}!"
-
-if __name__ == "__main__":
-    name = input("Enter your name: ")
-    print(greet(name))
+def main ():
+    return f"Hello deployment!"
